4046dc3ea0684b2c44e17a0b570c13a19b7e960e
RusveltMeza <101429716+RusveltMeza@users.noreply.github.com>
2023-09-18 23:04:28 -0500

    Add files via upload

---
+++ pooej7/POOEJ7.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pooej7;
+
+/**
+ *
+ * @author rusve
+ */
+public class POOEJ7 {
+
+    public static void main(String[] args) {
+        Formulario form = new Formulario();
+        form.setVisible(true);
+    }
+}
